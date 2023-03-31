cae7f6a0c8fbc609926b24dfc1197948c7e3813f
IsoyevDoston <110292294+IsoyevDoston@users.noreply.github.com>
2023-03-31 20:14:37 +0500

    Update app.py

--- bahona_topuvchi/app.py
+++ bahona_topuvchi/app.py
@@ -8,10 +8,29 @@ dp = Dispatcher(bot)
 EXCUSES_FILE = 'excuses.txt'
 excuses = []
 
-# Load the excuses from the file when the bot starts up
+# Bot ishga tushgani bilan excuses.txt ni ko'rsata boshlaydi
 with open(EXCUSES_FILE, 'r') as f:
     for line in f:
         excuses.append(line.strip())
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        "Salom, Men bilan turli vaziyatlarga mos bahonlar toping.\nBahona topish uchun /search ni bosing")
+
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    await message.reply("/start - botni qayta ishga tushirish\n/search - bahonalarni topish\n/add - bahona qo'shish")
+
+
+async def show(message: types.Message):
+    # Get the total number of items in the list
+    total_items = len(excuses)
+    # Send a message with the total number of items to the user
+    await message.answer(f"Hozirgacha ro'yhatda {total_items} ta bahona mavjud  ")
+
 
 async def search(message: types.Message):
     # Get a random element from the list
@@ -19,19 +38,28 @@ async def search(message: types.Message):
     # Send a message with the random element to the user
     await message.answer(random_element)
 
+
 async def add(message: types.Message):
     # Get the item to add from the message
     item = message.text.replace('/add ', '')
     # Add the item to the list
+
     excuses.append(item)
     # Write the updated list to the file
     with open(EXCUSES_FILE, 'a') as f:
         f.write(item + '\n')
     # Send a confirmation message to the user
-    await message.answer(f"{item} has been added to the list.")
+    await message.answer(f"Bu bahona ro'yhatga qo'shildi\n'{item}'  ")
+
+
+
+
+
 
 dp.register_message_handler(search, commands=['search'])
 dp.register_message_handler(add, commands=['add'])
+dp.register_message_handler(show, commands=['show'])
+
 
 if __name__ == '__main__':
     import asyncio
